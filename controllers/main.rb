08f357ed15f88621434e7a2bdc191a819dbaa11f
A-n-d-y-W-e-n <a409052003@gmail.com>
2017-05-03 15:33:38 +0800

    update views

--- controllers/main.rb
+++ controllers/main.rb
@@ -1,17 +1,19 @@
 # frozen_string_literal: true
+API_SERVER = 'http://localhost:9292'
 
 class LORAWAN_NMS_APP < Sinatra::Base
   get '/' do
-      slim :main
+    # username = 'andy'
+    # results = HTTP.get("#{API_SERVER}/app/#{username}")
+    slim :main
   end
 
-  get '/login/?' do
-      slim :login
+  get '/app/?' do
+    slim :app
   end
 
-
-  get '/nodes/?' do
-      slim :node
+  get '/gw/?' do
+    slim :gw
   end
 
 end
