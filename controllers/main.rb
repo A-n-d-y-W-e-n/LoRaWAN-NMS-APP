b1a1aeadb4d5468aecd5afadbfe3c74b387f4b16
A-n-d-y-W-e-n <a409052003@gmail.com>
2017-05-05 16:21:24 +0800

    updated views

--- controllers/main.rb
+++ controllers/main.rb
@@ -6,7 +6,7 @@ class LORAWAN_NMS_APP < Sinatra::Base
     slim :main
   end
 
-  post '/user/?' do
+  get '/user/?' do
     redirect "/#login"
   end
 
@@ -15,21 +15,41 @@ class LORAWAN_NMS_APP < Sinatra::Base
     password = params[:password]
     results = HTTP.get("#{API_SERVER}/app/#{username}")
     @data = JSON.parse(results.body)
-    slim :app
+    if @data[0]['app_name'] != nil
+      slim :app
+    else
+      redirect "/user"
+    end
   end
 
   get "/node/:username/:app_name/?" do
     username = params[:username]
     app_name = params[:app_name]
     results = HTTP.get("#{API_SERVER}/node/#{username}/#{app_name}")
-    @data = JSON.parse(results.body)
-    slim :node
+    if results.code == 200
+      @data = JSON.parse(results.body)
+      slim :node
+    end
   end
 
   get '/gw/?' do
     slim :gw
   end
 
+  get '/create_node/:username/:app_name/?' do
+    username = params[:username]
+    app_name = params[:app_name]
+    DevAddr = params[:DevAddr]
+    NwkSKey = params[:NwkSKey]
+    AppSKey = params[:AppSKey]
+    puts username
+    puts app_name
+    puts DevAddr
+    puts NwkSKey
+    puts AppSKey
+
+    redirect "/node/#{username}/#{app_name}/?"
+  end
 
 
 end
